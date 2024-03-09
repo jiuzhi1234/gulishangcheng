79ecd6dbba9a29fadfba1a516102cce43f38a04f
jiuzhi1234 <2745588458@qq.com>
2024-03-09 18:23:45 +0800

    Initial commit

---
+++ src/main/java/com/example/gulishangcheng/GulishangchengApplication.java
@@ -0,0 +1,13 @@
+package com.example.gulishangcheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulishangchengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulishangchengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gulishangcheng/GulishangchengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gulishangcheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulishangchengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
